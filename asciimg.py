5cc111e06e9ffa450c7a5ec4233be17e468d1a4b
dankeyy <dankeyy@protonmail.com>
2021-09-17 12:57:11 +0300

    made the conversion function lazier & refactor

--- asciimg.py
+++ asciimg.py
@@ -1,14 +1,16 @@
 from PIL import Image
 from argparse import ArgumentParser
 from operator import truediv
+from itertools import islice
 from os import linesep
 
 GSCALE = "@%#*+=-:. "
 WIDTH = 100
 SCALE = 0.45
+join = ''.join
 
 
-def resized(img):
+def resized(img): 
     ratio = 1 / truediv(*img.size)
     dimensions = ( WIDTH, int(ratio * WIDTH * SCALE) )
 
@@ -16,26 +18,28 @@ def resized(img):
 
 
 def converted(img):
-    img = resized( img.convert('L') ) # resized black and white pic
-    pixels = ''.join( GSCALE[x // 30] for x in img.getdata() ) # ascii chunk
+    ''' lazily sending out ascii rows '''
 
-    yield from ( pixels[i: i + WIDTH] + linesep for i in range(0, len(pixels), WIDTH) ) # full ascii pic
+    img = resized(img.convert('L')) # resized black and white pic
+    pixels = (GSCALE[x // 30] for x in img.getdata()) # ascii chunk
+
+    while line := join(islice(pixels, WIDTH)):
+        yield line + linesep
 
 
-def main():
+if __name__ == "__main__":
+    # argument handling stuff
     parser = ArgumentParser(description='image to ascii converter')
     parser.add_argument('--file', dest='inputfile', required=True)
     parser.add_argument('--out', dest='outputfile', required=False)
     arguments = parser.parse_args()
 
+    # opening source image for reading, picking output file
     input_img = Image.open(arguments.inputfile)
     output_img = arguments.outputfile or 'out.txt'
 
+    # writing
     with open(output_img, 'w') as f:
         for line in converted(input_img):
-            f.writelines(line)
+            f.write(line)
             print(line, end='')
-
-
-if __name__ == "__main__":
-    main()
